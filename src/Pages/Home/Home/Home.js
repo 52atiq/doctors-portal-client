a9021c7526be590f23945b6d885ef975bc3340f4
Atiqur Rahman <atiqit52@gmail.com>
2022-05-10 18:19:41 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import DentalCare from '../DentalCare/DentalCare';
+import Info from '../Info/Info';
+import Services from '../Services/Services';
+
+
+const Home = () => {
+    return (
+        <div className='px-12'>
+           <Banner></Banner>
+           <Info ></Info>
+           <Services> </Services>
+           <DentalCare> </DentalCare>
+        </div>
+    );
+};
+
+export default Home;
